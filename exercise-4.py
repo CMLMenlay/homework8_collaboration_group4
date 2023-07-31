dd726c2a80c2c8c2fde124b1f4f311c36061a9a4
CMLMenlay <2023212leng@aupp.edu.kh>
2023-07-31 23:06:00 +0700

    Update exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,21 @@
 def chunking_by(numbers, chunck):
     ...
+    # for i in range(0, len(numbers), chunck):
+    #     yield numbers[i:i + chunck ]
+    # final = list(chunking_by(numbers, chunck))
+    # return final
+# print (chunking_by([5, 4, 7, 3, 4, 5, 4], 3))
+
+    if not chunking_by:
+        return []
+    
+    return [numbers[i:i + chunck] for i in range(0, len(numbers), chunck)]
+
+insert_list = [5, 4, 7, 3, 4, 5, 4]
+insert_chunck = 3
+result = chunking_by(insert_list, insert_chunck)
+print (result)
+
+
+
+
