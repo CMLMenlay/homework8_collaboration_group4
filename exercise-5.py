46f29fda94374f99f556ea5426abf7c8dd5491b2
Sopanhataing <137533366+Sopanhataing@users.noreply.github.com>
2023-08-01 14:33:58 +0700

    update exercise 5

--- exercise-5.py
+++ exercise-5.py
@@ -2,6 +2,7 @@
 def reverse_ascending(numbers):
     result = []
     start = 0
+
     for n in range(len(numbers)):
 
         if numbers[n] <= numbers[n-1]:
@@ -10,5 +11,33 @@ def reverse_ascending(numbers):
             start = n
 
     return result + numbers[start:][::-1]
-    
 
+
+# assert list(reverse_ascending([1, 2, 3, 4, 5])) == [5, 4, 3, 2, 1]
+# assert list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])) == [
+#     10,
+#     7,
+#     5,
+#     4,
+#     8,
+#     7,
+#     2,
+#     3,
+#     1,
+# ]
+# assert list(reverse_ascending([5, 4, 3, 2, 1])) == [5, 4, 3, 2, 1]
+# assert list(reverse_ascending([])) == []
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
