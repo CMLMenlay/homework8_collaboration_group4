7dc6958ca358cb8fc8d9ef1795c4b21092b8dc3e
Sopanhataing <137533366+Sopanhataing@users.noreply.github.com>
2023-08-01 14:13:34 +0700

    exercise 5 complete

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,14 @@
+...
 def reverse_ascending(numbers):
-    ...
+    result = []
+    start = 0
+    for n in range(len(numbers)):
+
+        if numbers[n] <= numbers[n-1]:
+
+            result += ((numbers[start:n][::-1]))
+            start = n
+
+    return result + numbers[start:][::-1]
+    
+
